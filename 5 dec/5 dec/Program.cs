6727bfb3e13c1ffd3897edb5ca4e77a0e51925d9
Robin Bakker <robinb@live.nl>
2025-12-05 17:13:02 +0100

    5 dec - Finished
    Part 1 was done quick, the same I thought of Part 2. Until it kept running and didn't finish. I quickly noticed the memes on Reddit and it probably would've takes hours if not days/months to solve and petabytes of storage. Came up with a quicker algorithm, which took a while to refine as I missed an edge-case. Got it now though!

---
+++ 5 dec/5 dec/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _5_dec
+{
+    internal class Program
+    {
+        static List<(long, long)> rangesList = new List<(long, long)>();
+        static List<long> ingredientList = new List<long>();
+        static void Main(string[] args)
+        {
+            readFile();
+            var watch = Stopwatch.StartNew(); // Set stopwatch for tracking execution time in ms
+
+            partA();
+
+            partB2();
+
+
+            watch.Stop(); // Stop stopwatch and print
+            Console.WriteLine("Total execution time: " + watch.ElapsedMilliseconds);
+        }
+
+        static void partA()
+        {
+            var watch = Stopwatch.StartNew(); // Set stopwatch for tracking execution time in ms
+            int freshIngredients = 0;
+
+            foreach (long ingredient in ingredientList)
+            {
+                foreach ((long, long) range in rangesList)
+                {
+                    long start = range.Item1;
+                    long end = range.Item2;
+
+                    if (ingredient >= start && ingredient <= end)
+                    {
+                        freshIngredients++;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Part A: " + freshIngredients);
+
+            watch.Stop(); // Stop stopwatch and print
+            Console.WriteLine("Part A execution time: " + watch.ElapsedMilliseconds + "\n");
+        }
+
+
+        static void partB2()
+        {
+            var watch = Stopwatch.StartNew(); // Set stopwatch for tracking execution time in ms
+            long fresh = 0;
+
+            rangesList.Sort((a, b) => a.Item1.CompareTo(b.Item1));
+            for (int i = 0; i < rangesList.Count - 1; i++)
+            {
+
+                if (rangesList[i].Item2 >= rangesList[i + 1].Item1 && rangesList[i].Item2 < rangesList[i+1].Item2) // this.2 > next.1 && this.2 < next.2
+                {
+                    rangesList[i] = (rangesList[i].Item1, rangesList[i + 1].Item2);
+                    rangesList.RemoveAt(i + 1);
+                    i--;
+                }
+                else if (rangesList[i].Item2 >= rangesList[i + 1].Item1 && rangesList[i].Item2 >= rangesList[i + 1].Item2) // this.2 >= next.1 && this.2 >= next.2
+                {
+                    rangesList.RemoveAt(i + 1);
+                    i--;
+                }
+            }
+
+            foreach ((long, long) range in rangesList)
+            {
+                fresh += range.Item2 - range.Item1 + 1;
+            }
+
+            Console.WriteLine("Part B: " + fresh);
+
+            watch.Stop(); // Stop stopwatch and print
+            Console.WriteLine("Part B execution time: " + watch.ElapsedMilliseconds + "\n");
+        }
+
+        static void readFile()
+        {
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("C:\\Users\\Robin\\Documents\\GitHub\\AdventOfCode2025\\5 dec\\input.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+
+                bool ranges = true;
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    if (line == "")
+                    {
+                        ranges = false;
+                    }
+                    else if (ranges)
+                    {
+                        rangesList.Add((long.Parse(line.Split('-').First()), long.Parse(line.Split('-').Last())));
+                    }
+                    else
+                    {
+                        ingredientList.Add(long.Parse(line));
+                    }
+                        //write the line to console window
+                        //Read the next line
+                        line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
